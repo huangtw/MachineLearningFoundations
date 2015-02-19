27ae9f35816ca0c54153a50a6a07b46b4131fe95
huangtw <huangtw0422@gmail.com>
2015-02-20 00:42:51 +0800

    * Add “eta”. * Rename some functions and variables.

--- Homework01/pla.py
+++ Homework01/pla.py
@@ -1,36 +1,36 @@
 import sys, numpy, random
 
-def load_data(input_file_name):
-    input_data_list = []
-    f = open(input_file_name)
+def load_train_data(train_data_file_name):
+    train_data_list = []
+    f = open(train_data_file_name)
     for line in f.readlines():
         tmp = line.replace('\n', '').replace('\r', '').replace('\t', ' ').split(" ")
-        vector = [1.0]
+        x = [1.0]
 
         for i in range(len(tmp)):
             if i == len(tmp) - 1:
-                result = int(tmp[i])
+                y = int(tmp[i])
             else:
-                vector.append(float(tmp[i]))
+                x.append(float(tmp[i]))
 
-        input_data_list.append((numpy.array(vector), result))
-    return input_data_list
+        train_data_list.append((numpy.array(x), y))
+    return train_data_list
 
-def learn(weight_vector, input_data_list, order_type):
+def learn(weight_vector, train_data_list, order_type, eta):
     update_counter = 0
-    order = gen_order(order_type, len(input_data_list))
+    order = gen_order(order_type, len(train_data_list))
 
     while 1:
         mistake_counter = 0
         for i in order:
-            data = input_data_list[i]
+            data = train_data_list[i]
             if numpy.dot(weight_vector, data[0]) > 0:
                 result = 1
             else:
                 result = -1
 
             if result != data[1]:
-                weight_vector = weight_vector + data[1]*data[0]
+                weight_vector = weight_vector + eta * data[1] * data[0]
                 mistake_counter = mistake_counter + 1
 
         if mistake_counter == 0:
@@ -46,16 +46,18 @@ def gen_order(order_type, len):
         return range(len)
 
 def main():
-    input_file_name = sys.argv[1]
+    train_data_file_name = sys.argv[1]
     order_type = sys.argv[2]
     times = int(sys.argv[3])
+    eta = float(sys.argv[4])
+
     weight_vector = numpy.array([0.0, 0.0, 0.0, 0.0, 0.0])
 
-    input_data_list = load_data(input_file_name)
+    train_data_list = load_train_data(train_data_file_name)
 
     total_update = 0
     for i in range(times):
-        total_update = total_update + learn(weight_vector, input_data_list, order_type)
+        total_update = total_update + learn(weight_vector, train_data_list, order_type, eta)
 
     print "%d times, average = %f updates" % (times, total_update/times)
 
